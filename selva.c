681c2ea8dc7769b37fff6c3f6ca23175ee72a449
selvakumar33 <30343633+selvakumar33@users.noreply.github.com>
2017-08-11 16:57:51 +0530

    Create selva.c

---
+++ selva.c
@@ -0,0 +1,19 @@
+#include<stio.h>
+void main()
+{
+int n;
+printf("THE NUMBER IS"'/n);
+scanf(%d;&n);
+if(n>0)
+{
+printf("the number is positive ");
+}
+else
+{
+printf("the number is negtive");
+}
+else if(n=0)
+{
+printf("the number is zero");
+}
+}
